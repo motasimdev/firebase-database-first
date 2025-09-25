99c3a6483d8a323c18ba73f592e9ea66fcbfe545
motasimdev <themotasimdev@gmail.com>
2025-09-25 13:32:13 +0600

    adding

--- src/components/pages/Todo.jsx
+++ src/components/pages/Todo.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Container from "../Container";
-import { getDatabase, ref, set, push } from "firebase/database";
+import { getDatabase, ref, set, push, onValue } from "firebase/database";
 import {
   Table,
   TableBody,
@@ -27,11 +27,26 @@ const Todo = () => {
         {
           name: task,
         }
-          .then(alert("success"))
-          .catch("error")
+          // .then(alert("success"))
+          // .catch("error")
       );
     }
   };
+
+  // read data
+  const todotaskRef = ref(db, 'todotask/');
+  onValue(todotaskRef, (snapshot) => {
+  // const data = snapshot.val();
+  // updateStarCount(postElement, data);
+  // console.log(snapshot.val());
+  let arr = []
+  snapshot.forEach((item)=>{
+    console.log(item.val());
+    
+  })
+  
+});
+  // read data
   return (
     <>
       <div className="py-10 bg-blue-900">
@@ -56,10 +71,12 @@ const Todo = () => {
               Submit
             </button>
           </div>
-          <div className="overflow-x-auto">
+
+
+          <div className="overflow-x-auto mt-5">
             <Table striped>
               <TableHead>
-                <TableHeadCell>Product name</TableHeadCell>
+                <TableHeadCell>Task name</TableHeadCell>
                 <TableHeadCell>Color</TableHeadCell>
                 <TableHeadCell>Category</TableHeadCell>
                 <TableHeadCell>Price</TableHeadCell>
@@ -84,70 +101,7 @@ const Todo = () => {
                     </a>
                   </TableCell>
                 </TableRow>
-                <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                  <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                    Microsoft Surface Pro
-                  </TableCell>
-                  <TableCell>White</TableCell>
-                  <TableCell>Laptop PC</TableCell>
-                  <TableCell>$1999</TableCell>
-                  <TableCell>
-                    <a
-                      href="#"
-                      className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                    >
-                      Edit
-                    </a>
-                  </TableCell>
-                </TableRow>
-                <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                  <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                    Magic Mouse 2
-                  </TableCell>
-                  <TableCell>Black</TableCell>
-                  <TableCell>Accessories</TableCell>
-                  <TableCell>$99</TableCell>
-                  <TableCell>
-                    <a
-                      href="#"
-                      className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                    >
-                      Edit
-                    </a>
-                  </TableCell>
-                </TableRow>
-                <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                  <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                    Google Pixel Phone
-                  </TableCell>
-                  <TableCell>Gray</TableCell>
-                  <TableCell>Phone</TableCell>
-                  <TableCell>$799</TableCell>
-                  <TableCell>
-                    <a
-                      href="#"
-                      className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                    >
-                      Edit
-                    </a>
-                  </TableCell>
-                </TableRow>
-                <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                  <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                    Apple Watch 5
-                  </TableCell>
-                  <TableCell>Red</TableCell>
-                  <TableCell>Wearables</TableCell>
-                  <TableCell>$999</TableCell>
-                  <TableCell>
-                    <a
-                      href="#"
-                      className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                    >
-                      Edit
-                    </a>
-                  </TableCell>
-                </TableRow>
+                
               </TableBody>
             </Table>
           </div>
